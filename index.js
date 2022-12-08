de4053f825bbe81bd524ec170e3fc363641592b7
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-12-08 11:51:52 +0300

    Finished lab

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ function findMatching(array, name) {
   }
   fuzzyMatch(drivers,'Bobby')
 
+  
+
   //This function takes an array of driver objects and a string as arguments. Each driver object has two properties: 
   //name and hometown. The function should return each element whose name property matches the provided string argument.
 
